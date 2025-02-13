2d40cc5cba2dd774a694aa5b2ca7da5c26ecfcb8
coall132 <kaeligroche132@gmail.com>
2025-02-13 15:56:35 +0100

    test

--- autoML/autoML/settings.py
+++ autoML/autoML/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'autoML.urls'
